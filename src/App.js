e7d3ef607b70d7760a7dc8fab8a281d3f310289a
shubhamsingh83 <akashsingh832001@gmail.com>
2024-03-28 21:26:49 +0530

    flag

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
+import { useState,useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const [countries, setCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const getCountriesData = async () => {
+    try {
+      const data = await fetch("https://restcountries.com/v3.1/all");
+      const res = await data.json();
+      setCountries(res);
+    } catch (err) {
+      console.error("Error fetching data: ", err);
+    }
+  };
+
+  useEffect(() => {
+    getCountriesData();
+  }, []);
+  
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+    <h1>Country Flags</h1>
+    <input
+      className="searchInput"
+      type="text"
+      placeholder="Search countries..."
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
+    <div className="cardsContainer">
+      {filteredCountries.length > 0
+        ? filteredCountries.map((country) => (
+            <div className="countryCard" key={country.cca3}>
+              <img
+                className="countryImage"
+                src={country.flags.png}
+                alt={`Flag of ${country.name.common}`}
+              />
+              <h2 className="countryName">{country.name.common}</h2>
+            </div>
+          ))
+        : null}
     </div>
+  </div>
   );
 }
 
